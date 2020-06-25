d0ac3b48c32d4b6d26d36476bb1690296778b6c1
ssamko <ssamko@gdflab.com>
2020-06-25 13:46:28 +0900

    add: update hash size

--- hashkey_init.py
+++ hashkey_init.py
@@ -1,12 +1,18 @@
 # hashkey 생성
-import hashlib
+from hashlib import blake2b
 
 name = 'example_0624.avi'
 id = 1
 
-key = hashlib.sha256()
+key = blake2b(digest_size=10)
 key.update(name.encode('utf-8'))
 key.update(str(id).encode('utf-8'))
 
 print(key)
 print(key.digest())
+print(key.hexdigest())
+print(len(key.hexdigest()))
+
+hashkey = key.hexdigest()
+print(type(hashkey))
+print(len(hashkey))
