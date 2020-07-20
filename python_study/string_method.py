cf5bb11594aea22254da26186cbf1ada51230140
ssamko <ssamko@gdflab.com>
2020-07-20 10:24:38 +0900

    string method

--- python_study/string_method.py
+++ python_study/string_method.py
@@ -1,4 +1,10 @@
 # calculating string
 # frame_rate = videodata['@r_frame_rate'].split("/")[0]
+
 frame_rate = '30000/1001'
-print(eval(frame_rate))
+frame_rate = eval(frame_rate)
+# frame_rate = f'{frame_rate:.2f}'
+frame_rate = round(frame_rate, 2)
+
+print(type(frame_rate))
+print(frame_rate)
