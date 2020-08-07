b49fa77f09875ac6c375a3a94ffcb3e2f41a374f
ssamko <ssamko@gdflab.com>
2020-08-07 11:17:47 +0900

    coroutine

--- python_study/coroutine_study.py
+++ python_study/coroutine_study.py
@@ -6,10 +6,16 @@ async def say_after(delay, what):
     await asyncio.sleep(delay)
     print(what)
 
+async def count_up(count):
+    for i in range(count):
+        print(i+1)
+        await asyncio.sleep(0.7)
 
 async def main():
     task1 = asyncio.create_task(say_after(3, 'hello'))
+    task3 = asyncio.create_task(count_up(10))
     task2 = asyncio.create_task(say_after(1, 'world'))
+    task4 = asyncio.create_task(count_up(4))
 
     print(f'started at {time.strftime("%X")}')
 
