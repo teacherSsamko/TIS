041eec1871df30af97475a8c989caa5009d31494
ssamko <ssamko@gdflab.com>
2020-08-18 13:55:48 +0900

    smtp

--- python_study/smtp_study.py
+++ python_study/smtp_study.py
@@ -7,17 +7,20 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import private.config
 
 kakao = private.config.KAKAO_SMTP
+pikavue = private.config.PIKAVUE_SMTP
 
-
-smtp = smtplib.SMTP_SSL(host=kakao['host'], port=kakao['port'])
-
-smtp.login(kakao['id'],kakao['password'])
+smtp = smtplib.SMTP(host=pikavue['host'], port=pikavue['port'])
+# smtp = smtplib.SMTP_SSL(host=kakao['host'], port=kakao['port'])
+smtp.starttls()
+smtp.ehlo()
+smtp.login(pikavue['auth']['user'],pikavue['auth']['pass'])
 
 msg = MIMEText('hello pikavue')
 msg['Subject'] = 'Test'
 msg['To'] = 'ssamko@gdflab.com'
-msg['From'] = kakao['mail']
-smtp.sendmail(from_addr=kakao['mail'], to_addrs='ssamko@gdflab.com', msg=msg.as_string())
+msg['From'] = pikavue['auth']['user']
+smtp.sendmail(from_addr=pikavue['auth']['user'], to_addrs='ssamko@gdflab.com', msg=msg.as_string())
+# smtp.sendmail(from_addr=kakao['mail'], to_addrs='ssamko@gdflab.com', msg=msg.as_string())
 
 
 smtp.quit()
