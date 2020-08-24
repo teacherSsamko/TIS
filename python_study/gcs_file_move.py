ba46dd4db9c15de66eed0cc5ea41ef793efcf66a
ssamko <ssamko@gdflab.com>
2020-08-24 17:29:16 +0900

    fucntion for file move

--- python_study/gcs_file_move.py
+++ python_study/gcs_file_move.py
@@ -11,3 +11,20 @@ bucket = client.bucket("gdf-web-storage")
 blob = bucket.get_blob("video/develop/3LonEKEYUvphG3fie92pKo5969.mp4")
 
 bucket.rename_blob(blob, "video/failed/3LonEKEYUvphG3fie92pKo5969.mp4")
+
+def gcs_file_move(current_path, target_path):
+    bucket = client.bucket("gdf-web-storage")
+    blob = bucket.get_blob(current_path)
+
+    bucket.rename_blob(blob, target_path)
+
+def failed_file_move(current_path):
+    bucket = client.bucket("gdf-web-storage")
+    blob = bucket.get_blob(current_path)
+    filename = current_path.split("/")[-1]
+    target_path = f"video/failed/{filename}"
+
+    bucket.rename_blob(blob, target_path)
+
+
+    
