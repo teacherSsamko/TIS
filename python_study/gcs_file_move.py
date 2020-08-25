eb66685c2e45a5d390a4625dd9febaccf044a104
ssamko <ssamko@gdflab.com>
2020-08-25 13:38:27 +0900

    gcs file moving

--- python_study/gcs_file_move.py
+++ python_study/gcs_file_move.py
@@ -7,10 +7,11 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/PATH/TO/KEY.json"
 
 client = storage.Client()
 
-bucket = client.bucket("gdf-web-storage")
-blob = bucket.get_blob("video/develop/3LonEKEYUvphG3fie92pKo5969.mp4")
+bucket = client.get_bucket("gdf-web-storage")
+blob = bucket.get_blob("video/")
 
-bucket.rename_blob(blob, "video/failed/3LonEKEYUvphG3fie92pKo5969.mp4")
+
+bucket.rename_blob(blob, "video/failed/mp4")
 
 def gcs_file_move(current_path, target_path):
     bucket = client.bucket("gdf-web-storage")
