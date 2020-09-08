995d3db1a13cf389ad4c9aec769dd21bf0786134
ssamko <ssamko@gdflab.com>
2020-09-08 11:21:14 +0900

    video downloader with progress bar

--- crawler/video_downloader.py
+++ crawler/video_downloader.py
@@ -1,12 +1,14 @@
 import os
+import sys
 import urllib.request
 
 from pymongo import MongoClient
+import requests
 
 
 mongo = MongoClient("mongodb://localhost:27017")
 db = mongo['aircode']
-col = db['ssg_prod']
+col = db['lotte_prod']
 
 prod_list = list(col.find())
 
@@ -16,7 +18,8 @@ prod_list = list(col.find())
 #     for url in urls:
 #         urllib.request.urlretrieve(url, f"./images/{directory}/{i}.jpg")
 #         i += 1
-
+total_count = len(prod_list)
+current_no = 1
 for prod in prod_list:
     try:
         video_url = prod['video_url']
@@ -24,6 +27,22 @@ for prod in prod_list:
         print(prod_id)
     except:
         continue
-    print('start download')
-    urllib.request.urlretrieve(video_url, f"crawler/ssg/videos/{prod_id}.mp4")
-    print('download finisehd')
+    print(f'start download {current_no} / {total_count}')
+    # urllib.request.urlretrieve(video_url, f"crawler/lottemall/videos/{prod_id}.mp4")
+    file_name = f"crawler/lottemall/videos/{prod_id}.mp4"
+    with open(file_name, "wb") as f:
+        response = requests.get(video_url, stream=True)
+        total_length = response.headers.get('content-length')
+        if total_length is None: # no content length header
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
+                sys.stdout.flush()
+    print('\ndownload finished')
+    current_no += 1
