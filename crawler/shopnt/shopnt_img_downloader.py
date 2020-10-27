e316880d95eb812ba09620a9899dca8998e8aa31
ssamko <ssamko@gdflab.com>
2020-10-27 14:13:45 +0900

    shopnt img downloader

--- crawler/shopnt/shopnt_img_downloader.py
+++ crawler/shopnt/shopnt_img_downloader.py
@@ -10,9 +10,9 @@ mongo = MongoClient("mongodb://localhost:27017")
 db = mongo['aircode']
 col = db['shopnt_prod']
 
-prod_list = list(col.find())
-
 today = datetime.date.today()
+
+prod_list = list(col.find({'reg_date':'2020-10-07'}))
 
 if not os.path.exists(os.path.join(BASE_DIR, 'images')):
     os.mkdir(os.path.join(BASE_DIR, 'images'))
@@ -21,7 +21,16 @@ if not os.path.exists(os.path.join(BASE_DIR, f'images/{today}')):
     os.mkdir(os.path.join(BASE_DIR, f'images/{today}'))
 
 i = 0
+stop = 236 + 247 + 31 + 197 + 153 + 32 + 183 + 15
+# total = len(prod_list) - stop - 1
+total = len(prod_list)
+
 for prod in prod_list:
     img_url = prod['img_url']
-    urllib.request.urlretrieve(img_url, os.path.join(BASE_DIR,f"images/{today}/{i}.jpg"))
+    if prod['prod_id'][:-2] != '':
+        prod_id = prod['prod_id'][:-2]
+        print(prod_id)
+        prod_id = int(prod_id)
+        urllib.request.urlretrieve(img_url, os.path.join(BASE_DIR,f"images/{today}/{prod_id}.jpg"))
+        print(f'{i}/{total}')
     i += 1
