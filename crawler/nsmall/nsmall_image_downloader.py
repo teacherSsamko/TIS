c0d2984bd062d181ddeebb5b3ed75a6c8c7e0a9b
ssamko <ssamko@gdflab.com>
2020-11-06 13:31:27 +0900

    nsmall_main script

--- crawler/nsmall/nsmall_image_downloader.py
+++ crawler/nsmall/nsmall_image_downloader.py
@@ -4,26 +4,28 @@ import datetime
 
 from pymongo import MongoClient
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-mongo = MongoClient("mongodb://localhost:27017")
-db = mongo['aircode']
-# review_col = db['nsmall_reviews']
-prod_col = db['nsmall']
-today = datetime.date.today()
 
-prod_list = list(prod_col.find({'reg_date':str(today)}))
+def main():
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    mongo = MongoClient("mongodb://localhost:27017")
+    db = mongo['aircode']
+    # review_col = db['nsmall_reviews']
+    prod_col = db['nsmall']
+    today = datetime.date.today()
 
-if not os.path.exists(os.path.join(BASE_DIR, f'images/{today}')):
-    os.mkdir(os.path.join(BASE_DIR, f'images/{today}'))
+    prod_list = list(prod_col.find({'reg_date':str(today)}))
 
-img_dir = os.path.join(BASE_DIR, 'images')
+    if not os.path.exists(os.path.join(BASE_DIR, f'images/{today}')):
+        os.mkdir(os.path.join(BASE_DIR, f'images/{today}'))
 
-total = len(prod_list)
-i = 1
+    img_dir = os.path.join(BASE_DIR, 'images')
 
-for prod in prod_list:
-    print(f'{i}/{total}')
-    prod_id = prod['prod_id']
-    img_url = prod['img_url']
-    urllib.request.urlretrieve(img_url, f"{img_dir}/{today}/{prod_id}.jpg")
-    i += 1
+    total = len(prod_list)
+    i = 1
+
+    for prod in prod_list:
+        print(f'{i}/{total}')
+        prod_id = prod['prod_id']
+        img_url = prod['img_url']
+        urllib.request.urlretrieve(img_url, f"{img_dir}/{today}/{prod_id}.jpg")
+        i += 1
---
+++ crawler/nsmall/nsmall_main.py
@@ -0,0 +1,6 @@
+import nsmall_api
+import nsmall_image_downloader
+
+if __name__ == "__main__" :
+    nsmall_api.main()
+    nsmall_image_downloader.main()
