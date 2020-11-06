43109aa45cead13b6ffacdeb2284c60ed5b6529f
ssamko <ssamko@gdflab.com>
2020-11-06 13:31:40 +0900

    timeout error handling

--- crawler/nsmall/nsmall_review.py
+++ crawler/nsmall/nsmall_review.py
@@ -9,18 +9,27 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from pymongo import MongoClient
 
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 mongo = MongoClient("mongodb://localhost:27017")
 db = mongo['aircode']
 review_col = db['nsmall_reviews']
 prod_col = db['nsmall']
 today = datetime.date.today()
-prod_list = list(prod_col.find('reg_date':str(today)))
+prod_list = list(prod_col.find({'reg_date':str(today)}))
 print(len(prod_list))
 review_dataset = []
 
 options = Options()
 options.page_load_strategy = 'eager'
 driver = webdriver.Chrome(options=options,executable_path="/Users/ssamko/Downloads/chromedriver")
+
+if not os.path.exists(os.path.join(BASE_DIR, f'daily')):
+    os.mkdir(os.path.join(BASE_DIR, f'daily'))
+
+daily_index_dir = os.path.join(BASE_DIR, f'daily/{today}')
+
+if not os.path.exists(daily_index_dir):
+    os.mkdir(daily_index_dir)
 
 def get_last_page(driver):
     try:
@@ -33,54 +42,90 @@ def get_last_page(driver):
 
     return last_page
 
+index_file = os.path.join(daily_index_dir, f'{today}_index.txt')
+
+def get_start_idx():
+    if not os.path.exists(index_file):
+        return 0
+    with open(index_file, 'r') as f:
+        last_idx = f.readlines()[-1]
+        print(f'last_idx >> {last_idx}')
+        if 'ok' in last_idx:
+            return -1
+        return int(last_idx)
 
 
-for prod in prod_list:
-    print('INDEX ',prod_list.index(prod))
-    # if prod['ctg'] != '푸드' and prod['prod_id'] != '29860083':
-    if prod['ctg'] != '푸드':
-        url = f'http://www.nsmall.com/ProductDisplay?busChnId=INT&langId=-9&storeId=13001&partNumber={prod["prod_id"]}&menuUri=NSItemDetailView'
-    # driver = webdriver.Chrome(executable_path="/Users/ssamko/Downloads/chromedriver")
-    # url = 'http://www.nsmall.com/ProductDisplay?busChnId=INT&langId=-9&storeId=13001&partNumber=29793300&menuUri=NSItemDetailView'
-        driver.get(url)
-        last_page = get_last_page(driver)
-        if last_page:
-            current_page = driver.find_element_by_xpath('//*[@id="review"]/strong')
-            cur_page_no = int(current_page.text)
-        prod_name = driver.find_element_by_css_selector('span.inform.pr10').text
-        prod_id = driver.find_element_by_css_selector('p.itemNo > span').text.strip()
-        # driver.find_element_by_css_selector('#tbodyReviewList > tr:nth-child(7) > td.tle > div > p > a').click()
-        while last_page != 0 and cur_page_no != 1:
-            current_page = driver.find_element_by_xpath('//*[@id="review"]/strong')
-            cur_page_no = int(current_page.text)
-            # review = driver.find_element_by_css_selector('#trReviewContents_3 > td > div > div.cont > div.txt > p:nth-child(1)').text
-            
-            review_list = driver.find_elements_by_css_selector('tr.reply > td > div > div.cont > div.txt > p:nth-child(1)')
-            review_titles = driver.find_elements_by_css_selector('td.tle > div > p > a')
-            stars = driver.find_elements_by_css_selector('td.star_area > span > em')
-            review_ids = driver.find_elements_by_css_selector('td.fb')
-            # print(len(review_list))
-            print(f'[{prod_id}]{prod_name}')
-            for idx,review in enumerate(review_list):
-                review_titles[idx].click()
-                review_id = review_ids[idx*2].text
-                score = stars[idx].text[:-1]
-                if not score:
-                    score = 0
-                score = int(score)
-                print(f'{idx}({score}) - {review.text}')
-                db_data = {
-                    'prod_id':prod['prod_id'],
-                    'prod_name':prod_name,
-                    'prod_review_id':review_id,
-                    'score':score,
-                    'review':review.text,
-                    'reg_date': str(today)
-                }
-                review_dataset.append(db_data)
-                review_col.insert_one(db_data)
-            if cur_page_no != 1:
-                current_page.find_elements_by_xpath('preceding-sibling::a[@href]')[-1].click()
-driver.quit()
+def crawl_from(start_idx):
+    with open(index_file, 'a') as f:
+        f.write('\n')
+        for prod in prod_list[start_idx:]:
+            prod_idx = prod_list.index(prod)
+            print('INDEX ',prod_idx)
+            f.write(f'{prod_idx}')
+            # if prod['ctg'] != '푸드' and prod['prod_id'] != '29860083':
+            if prod['ctg'] != '푸드':
+                url = f'http://www.nsmall.com/ProductDisplay?busChnId=INT&langId=-9&storeId=13001&partNumber={prod["prod_id"]}&menuUri=NSItemDetailView'
+            # driver = webdriver.Chrome(executable_path="/Users/ssamko/Downloads/chromedriver")
+            # url = 'http://www.nsmall.com/ProductDisplay?busChnId=INT&langId=-9&storeId=13001&partNumber=29793300&menuUri=NSItemDetailView'
+                driver.get(url)
+                last_page = get_last_page(driver)
+                
+                if last_page:
+                    current_page = driver.find_element_by_xpath('//*[@id="review"]/strong')
+                    cur_page_no = int(current_page.text)
+                prod_name = driver.find_element_by_css_selector('span.inform.pr10').text
+                prod_id = driver.find_element_by_css_selector('p.itemNo > span').text.strip()
+                # driver.find_element_by_css_selector('#tbodyReviewList > tr:nth-child(7) > td.tle > div > p > a').click()
+                while last_page != 0 and cur_page_no != 1:
+                    current_page = driver.find_element_by_xpath('//*[@id="review"]/strong')
+                    cur_page_no = int(current_page.text)
+                    # review = driver.find_element_by_css_selector('#trReviewContents_3 > td > div > div.cont > div.txt > p:nth-child(1)').text
+                    
+                    review_list = driver.find_elements_by_css_selector('tr.reply > td > div > div.cont > div.txt > p:nth-child(1)')
+                    review_titles = driver.find_elements_by_css_selector('td.tle > div > p > a')
+                    stars = driver.find_elements_by_css_selector('td.star_area > span > em')
+                    review_ids = driver.find_elements_by_css_selector('td.fb')
+                    # print(len(review_list))
+                    print(f'[{prod_id}]{prod_name}')
+                    for idx,review in enumerate(review_list):
+                        review_titles[idx].click()
+                        review_id = review_ids[idx*2].text
+                        score = stars[idx].text[:-1]
+                        if not score:
+                            score = 0
+                        score = int(score)
+                        # print(f'{idx}({score}) - {review.text}')
+                        db_data = {
+                            'prod_id':prod['prod_id'],
+                            'prod_name':prod_name,
+                            'prod_review_id':review_id,
+                            'score':score,
+                            'review':review.text,
+                            'reg_date': str(today)
+                        }
+                        review_dataset.append(db_data)
+                        review_col.insert_one(db_data)
+                    if cur_page_no != 1:
+                        current_page.find_elements_by_xpath('preceding-sibling::a[@href]')[-1].click()
+            f.write(f'\tok\n')
+        driver.quit()
 
 # review_col.insert_many(review_dataset)
+
+start_idx = get_start_idx()
+
+while True:
+    try:
+        crawl_from(start_idx)
+        print('finish?')
+        break
+    except:
+        print('timeout error: stop')
+        time.sleep(2)
+        driver.quit()
+        driver = webdriver.Chrome(options=options,executable_path="/Users/ssamko/Downloads/chromedriver")
+        start_idx = get_start_idx()
+        if start_idx == -1:
+            print('finish well')
+            break
+        continue
