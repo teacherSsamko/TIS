367e795764dd3ba5fe0374342e8b9b6c218b61c8
ssamko <ssamko@gdflab.com>
2020-12-07 15:22:37 +0900

    feat: get gs detail image

--- crawler/gs/gs_detail.py
+++ crawler/gs/gs_detail.py
@@ -17,10 +17,16 @@ options = Options()
 options.page_load_strategy = 'eager'
 driver = webdriver.Chrome(options=options,executable_path="/Users/ssamko/Downloads/chromedriver")
 
-with open('crawler/gs/url_list.txt','r') as f:
-    urls = f.readlines()[9:]
+today = datetime.date.today()
 
+with open('crawler/gs/url_list.txt','r') as f:
+    urls = f.readlines()
+
+total = len(urls) - 1
+
+start_t = datetime.datetime.now()
 for url in urls:
+    print(f'{urls.index(url)} / {total}')
     driver.get(url)
     # time.sleep(2)
     prod_id = url.split("=")[1].split("&")[0]
@@ -30,7 +36,19 @@ for url in urls:
     except:
         continue
     img_url = driver.find_element_by_css_selector('a.btn_img > img').get_attribute('src')
+    #prdDetailIfr
+    driver.switch_to.frame('prdDetailIfr')
+    #html2image > p > img
+    details = driver.find_elements_by_css_selector('#html2image > p')
+    # print('ppppp >>>', details)
+    # print('length of ppppp >>>', len(details))
+    detail_img_url = []
+    for d in details:
+        img = d.find_elements_by_css_selector('img')
+        while img:
+            detail_img_url.append(img.pop().get_attribute('src'))
     
+    driver.switch_to.parent_frame()
     try:
         score = driver.find_element_by_css_selector('span.customer-reviews-score > em').text
         score_persons = driver.find_element_by_css_selector('a.customer-reviews-link-count > em').text.strip("()")
@@ -45,12 +63,16 @@ for url in urls:
         'score': score,
         'score_persons': score_persons,
         'img_url':img_url,
+        'detail_img_url':detail_img_url,
+        'reg_date':str(today)
     }
-    print(prod_id)
-    print(prod_name)
-    print()
+    # print(prod_id)
+    # print(prod_name)
+    # print(detail_img_url)
+    # print()
     col.insert_one(db_data)
     # break
     # time.sleep(2)
 
 driver.quit()
+print(f'runtime >> {datetime.datetime.now() - start_t}')
