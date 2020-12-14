f9cce1e1b4d640e06826efda7cbc95d4f74e6dfb
ssamko <ssamko@gdflab.com>
2020-12-14 10:52:50 +0900

    api method

--- crawler/nsmall/detail_api.py
+++ crawler/nsmall/detail_api.py
@@ -1,6 +1,8 @@
 import json
+import html
 
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://www.nsmall.com/NSItemDetailGoodsGuide?catalogId=97001&langId=-9&storeId=13001'
 data = {
@@ -8,4 +10,22 @@ data = {
 }
 result = requests.get(url, data)
 print(result)
-print(result.json())
+detail_val = result.json()['jsonData']['goodsGuideDataList'][0]['specsInputVal']
+print(detail_val)
+detail_html = html.unescape(detail_val)
+print()
+detail_soup = BeautifulSoup(detail_html, 'html.parser')
+print(detail_soup.select_one('img')['src'])
+
+def get_detail_img_url(prod_id):
+    url = 'http://www.nsmall.com/NSItemDetailGoodsGuide?catalogId=97001&langId=-9&storeId=13001'
+    # http://www.nsmall.com/NSItemDetailGoodsGuide?catalogId=97001&langId=-9&storeId=13001
+    # "cmdType":7,"catentryId":"30214022","busChnId":"CTCOM","deviceChnId":"INT"
+    data = {
+        "cmdType":7,"catentryId":f"{prod_id}","busChnId":"CTCOM","deviceChnId":"INT"
+    }
+    result = requests.get(url, data)
+    detail_val = result.json()['jsonData']['goodsGuideDataList'][0]['specsInputVal']
+    detail_html = html.unescape(detail_val)
+    detail_soup = BeautifulSoup(detail_html, 'html.parser')
+    return detail_soup.select_one('img')['src']
